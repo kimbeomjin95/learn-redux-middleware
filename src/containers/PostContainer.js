37babe1de0189caae648fee1ef4bb389969a5e3a
Kim Beom Jin <planet1995@nate.com>
2021-06-30 21:33:20 +0900

    [add] 사용자 경험 개선 B - 포스트 데이터 상태 구조 바꾸기

--- src/containers/PostContainer.js
+++ src/containers/PostContainer.js
@@ -2,25 +2,27 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Post from '../components/Post';
+import { reducerUtils } from '../lib/asyncUtils';
 import { clearPost, getPost } from '../modules/posts';
 
 function PostContainer({ postId }) {
   // postId: 라우터 파라미터를 통해 받아 올 것
 
-  const { data, loading, error } = useSelector(state => state.posts.post);
+  // 비구조화 할당하는 과정에서 최초로 렌더링될 때 state.posts.post[postId] 값이 존재하지 않을 때를 위한 오류 방지
+  const { data, loading, error } = useSelector(
+    state => state.posts.post[postId] || reducerUtils.initial(),
+  );
   const dispatch = useDispatch();
 
   // 컴포넌트가 처음 렌더링될 때 thunk생성함수(getPost )를 사용하기 위해서 useEffect를 사용
   useEffect(() => {
+    // 데이터가 이미 있다면 요청조차 하지 않기 위함
+    if (data) return;
     dispatch(getPost(postId));
-    return () => {
-      // cleanup: 업데이트 되기 직전 호출(컴포넌트가 화면에서 사라질때, postId가 바뀔 때
-      dispatch(clearPost());
-    };
   }, [postId, dispatch]);
 
   // 결과에 따라 다른 화면
-  if (loading) return <div>로딩중...</div>;
+  if (loading && !data) return <div>로딩중...</div>;
   if (error) return <div>에러 발생!</div>;
   if (!data) return null;
 
--- src/modules/posts.js
+++ src/modules/posts.js
@@ -14,24 +14,76 @@ const GET_POST = 'GET_POST';
 const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
 const GET_POST_ERROR = 'GET_POST_ERROR';
 
-// // 포스트 비우기
+// 포스트 비우기
 const CELAR_POST = 'CELAR_POST';
 
 // 액션 생성 함수를 만들지 않고 thunk에서 직접 dispatch 할 수 있음
 // createPromiseThunk 생성 함수
 export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
-export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
+
+// post 데이터 상태구조 변경을 위한 thunk함수 생성(getPost를 위한 thunk함수 생성완료)
+export const getPost = id => async dispatch => {
+  dispatch({ type: GET_POST, meta: id }); //요 meta값에 id를 전달해주면 reducer에서 id를 참고해서 상태를 udpate 함
+  try {
+    const payload = await postsAPI.getPostById(id); // payload: posts, post의 이름을 payload로 통합
+    dispatch({ type: GET_POST_SUCCESS, payload, meta: id }); // API 청이 성공했다는걸 의미하는 dispatch
+  } catch (e) {
+    dispatch({
+      type: GET_POST_ERROR,
+      payload: e,
+      error: true,
+      meta: id,
+    });
+  }
+};
 export const clearPost = () => ({ type: CELAR_POST });
 
 // 초기값
 const initialState = {
   posts: reducerUtils.initial(),
-  post: reducerUtils.initial(),
+  post: {},
 };
 
 // handleAsyncActoins
 const getPostsReducer = handleAsyncActoins(GET_POSTS, 'posts', true);
-const getPostReducer = handleAsyncActoins(GET_POST, 'post');
+
+// 직접 reducer 작성
+const getPostReducer = (state, action) => {
+  const id = action.meta;
+  switch (action.type) {
+    case GET_POST:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          // 특정 아이디(key)를 사용해서 로딩중 상태 만들기
+          // 기존의 불러온 상태가 있다면 그 상태를 재사용하기 위함(로딩중에 데이터 유지)
+          // 최초 특정 포스트를 불러올때 state.post[id]값이 undefined이므로 data를 조회하면 에러가 발생하므로 이를 방지하기 위해 조건 추가
+          [id]: reducerUtils.loading(state.post[id] && state.post[id].data),
+        },
+      };
+    case GET_POST_SUCCESS:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          // 특정 아이디(key)를 사용해서 로딩중 상태 만들기
+          // 기존의 불러온 상태가 있다면 그 상태를 재사용하기 위함(로딩중에 데이터 유지)
+          // 최초 특정 포스트를 불러올때 state.post[id]값이 undefined이므로 data를 조회하면 에러가 발생하므로 이를 방지하기 위해 조건 추가
+          [id]: reducerUtils.loading(action.payload),
+        },
+      };
+    case GET_POST_ERROR:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          [id]: reducerUtils.error(action.payload),
+        },
+      };
+    default:
+  }
+};
 
 // handleAsyncActoins reducer
 export default function posts(state = initialState, action) {
