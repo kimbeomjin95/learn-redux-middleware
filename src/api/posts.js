8fb7306c94171a5f495d598339eb7b2269116116
Kim Beom Jin <planet1995@nate.com>
2021-07-03 16:02:42 +0900

    [add] CORS and Proxy

--- src/api/posts.js
+++ src/api/posts.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 // 가짜 API 함수
 export const getPosts = async () => {
-  const response = await axios.get('http://localhost:4000/posts');
+  const response = await axios.get('/posts'); // axios를 사용할 때 도메인을 지우면 현재 react-application이 띄어져 있는 주소의 도메인을 사용
   return response.data;
 };
 
 export const getPostById = async id => {
-  const response = await axios.get(`http://localhost:4000/posts/${id}`);
+  const response = await axios.get(`/posts/${id}`);
   return response.data;
 };
